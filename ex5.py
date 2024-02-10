eda0dd3abc0e02f069e365aa9afc8c12874e2c3e
Ohad-Alon <149534611+Ohad-Alon@users.noreply.github.com>
2024-02-10 17:56:00 +0200

    Maybe now it works?

--- ex5.py
+++ ex5.py
@@ -50,53 +50,53 @@ class Polymerase(Enzyme):
 
 class Mutase(Enzyme):
     def __init__(self, freq):
-        self.freq = freq
+        self.m_freq = freq
     
     def process(self, dna_sequence):
         res_sequence = dna_sequence.get_sequence()
-        for i in range(self.freq-1, res_sequence.get_length(), self.freq):
+        for i in range(self.m_freq - 1, len(res_sequence), self.m_freq):
             res_sequence[i] = Complement(res_sequence[i])
         dna_sequence.replace_sequence(res_sequence)
         return dna_sequence
 
 class CRISPR:
     def __init__(self, seq):
-        self.seq = seq
+        self.m_seq = seq
 
     def process(self, dna_sequence):
-        i = dna_sequence.find_alignment(self.seq)
+        i = dna_sequence.find_alignment(self.m_seq)
         while i != -1:
             dna_sequence.replace_sequence(
                 dna_sequence.get_sequence()[:i] +
                 ['W'] +
-                dna_sequence.get_sequence()[i+len(self.seq):]
+                dna_sequence.get_sequence()[i+len(self.m_seq):]
             )
-            i = dna_sequence.find_alignment(self.seq)
+            i = dna_sequence.find_alignment(self.m_seq)
         return dna_sequence
 
 class CRISPR_Cas9(CRISPR):
     def __init__(self, seq, new_seq):
         super().__init__(seq)
-        self.new_seq = new_seq
+        self.m_new_seq = new_seq
 
     def process(self, dna_sequence):
         dna_sequence = super().process(dna_sequence)
         # Replace every W with new_seq
-        i = dna_sequence.find_alignment(['W'])
+        i = dna_sequence.find_alignment("W")
         while i != -1:
             dna_sequence.replace_sequence(
                 dna_sequence.get_sequence()[:i] +
-                self.new_seq +
-                dna_sequence.get_sequence()[i+len(self.seq):]
+                DNASequence(self.m_new_seq).get_sequence() +
+                dna_sequence.get_sequence()[i+len(self.m_seq):]
             )
-            i = dna_sequence.find_alignment(['W'])
+            i = dna_sequence.find_alignment("W")
         # Replace every M with the complement of new_seq
-        j = dna_sequence.find_alignment(['M'])
+        j = dna_sequence.find_alignment("M")
         while j != -1:
             dna_sequence.replace_sequence(
                 dna_sequence.get_sequence()[:j] +
-                DNASequence(self.new_seq).get_complement() +
-                dna_sequence.get_sequence()[i+len(self.seq):]
+                DNASequence(self.m_new_seq).get_complement() +
+                dna_sequence.get_sequence()[i+len(self.m_seq):]
             )
-            j = dna_sequence.find_alignment(['W'])
+            j = dna_sequence.find_alignment("W")
         return dna_sequence
