b4ea67196e40ca6772eba774ffdc5e2972e7d38e
Ohad-Alon <149534611+Ohad-Alon@users.noreply.github.com>
2024-02-10 18:02:34 +0200

    OK, so not m_

--- ex5.py
+++ ex5.py
@@ -14,28 +14,28 @@ def Complement(nucleotide):
 
 class DNASequence:
     def __init__(self, nucleotides):
-        self.m_nucleotides = [nucleotide for nucleotide in nucleotides]
+        self.nucleotides = [nucleotide for nucleotide in nucleotides]
 
     def get_sequence(self):
-        return self.m_nucleotides
+        return self.nucleotides
     
     def get_length(self):
-        return len(self.m_nucleotides)
+        return len(self.nucleotides)
     
     def get_complement(self):
-        return [Complement(nucleotide) for nucleotide in self.m_nucleotides] #1st one-liner
+        return [Complement(nucleotide) for nucleotide in self.nucleotides] #1st one-liner
     
     def get_nucleotide(self, index):
-        return self.m_nucleotides[index]
+        return self.nucleotides[index]
     
     def find_alignment(self, seq):
         nucleotide_str = ""
-        for nucleotide in self.m_nucleotides:
+        for nucleotide in self.nucleotides:
             nucleotide_str += nucleotide
         return nucleotide_str.find(seq)
     
     def replace_sequence(self, seq):
-        self.m_nucleotides = seq.copy() #2nd one-liner
+        self.nucleotides = seq.copy() #2nd one-liner
 
 class Enzyme:
     def process(self, dna_sequence):
@@ -47,34 +47,34 @@ class Polymerase(Enzyme):
 
 class Mutase(Enzyme):
     def __init__(self, freq):
-        self.m_freq = freq
+        self.freq = freq
     
     def process(self, dna_sequence):
         res_sequence = dna_sequence.get_sequence()
-        for i in range(self.m_freq - 1, len(res_sequence), self.m_freq):
+        for i in range(self.freq - 1, len(res_sequence), self.freq):
             res_sequence[i] = Complement(res_sequence[i])
         dna_sequence.replace_sequence(res_sequence)
         return dna_sequence
 
 class CRISPR:
     def __init__(self, seq):
-        self.m_seq = seq
+        self.seq = seq
 
     def process(self, dna_sequence):
-        i = dna_sequence.find_alignment(self.m_seq)
+        i = dna_sequence.find_alignment(self.seq)
         while i != -1:
             dna_sequence.replace_sequence(
                 dna_sequence.get_sequence()[:i] +
                 ['W'] +
-                dna_sequence.get_sequence()[i+len(self.m_seq):]
+                dna_sequence.get_sequence()[i+len(self.seq):]
             )
-            i = dna_sequence.find_alignment(self.m_seq)
+            i = dna_sequence.find_alignment(self.seq)
         return dna_sequence
 
 class CRISPR_Cas9(CRISPR):
     def __init__(self, seq, new_seq):
         super().__init__(seq)
-        self.m_new_seq = new_seq
+        self.new_seq = new_seq
 
     def process(self, dna_sequence):
         dna_sequence = super().process(dna_sequence)
@@ -83,8 +83,8 @@ class CRISPR_Cas9(CRISPR):
         while i != -1:
             dna_sequence.replace_sequence(
                 dna_sequence.get_sequence()[:i] +
-                DNASequence(self.m_new_seq).get_sequence() +
-                dna_sequence.get_sequence()[i+len(self.m_seq):]
+                DNASequence(self.new_seq).get_sequence() +
+                dna_sequence.get_sequence()[i+len(self.seq):]
             )
             i = dna_sequence.find_alignment("W")
         # Replace every M with the complement of new_seq
@@ -92,8 +92,8 @@ class CRISPR_Cas9(CRISPR):
         while j != -1:
             dna_sequence.replace_sequence(
                 dna_sequence.get_sequence()[:j] +
-                DNASequence(self.m_new_seq).get_complement() +
-                dna_sequence.get_sequence()[i+len(self.m_seq):]
+                DNASequence(self.new_seq).get_complement() +
+                dna_sequence.get_sequence()[i+len(self.seq):]
             )
             j = dna_sequence.find_alignment("W")
         return dna_sequence
